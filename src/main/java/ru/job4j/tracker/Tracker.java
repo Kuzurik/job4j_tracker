7c44423a73207dc79806c3db03e1f62e85a94e98
kuzurik <vorota-24@bk.ru>
2021-07-07 22:14:31 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,24 +28,22 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean rsl = false;
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             items[index] = item;
             items[index].setId(id);
-            rsl = true;
         }
         return rsl;
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean rsl = false;
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, items.length - index - 1);
             items[size - 1] = null;
             size--;
-            rsl = true;
         }
         return rsl;
     }
