b061b355401dc1e2a4ce18b8183f6165012372f7
kuzurik <vorota-24@bk.ru>
2021-07-14 21:54:46 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -23,6 +23,15 @@ public class StartUI {
                 for (Item item : items) {
                 System.out.println(item);
                 }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ====");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+                System.out.println("Заявка изменена успешно.");
             } else if (select == 6) {
                 run = false;
             }
