9803c7d7cb5e9bfbdf2a36bde8e765c86f62e21b
kuzurik <vorota-24@bk.ru>
2021-07-21 09:19:35 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNumberNLessThanZero() {
+        Fact fact = new Fact();
+        fact.calc(-1);
+    }
+}
