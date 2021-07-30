93409016b07d4ae33f221d72aee9a7b7180d956b
kuzurik <vorota-24@bk.ru>
2021-07-30 22:47:53 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,12 +5,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+    /**
+    * Class describes work bank system and transfer money from account
+    * to account.
+    * @author Kuzura Alexandr
+    * @version 1.0
+    */
+
 public class BankService {
+
+    /**
+     * Collection map keep Users and list of accounts every user.
+     */
+
     private final Map<User, List<Account>> users = new HashMap<>();
+
+    /**
+     * Method adds the user to map.
+     * @param user the user which adds to map
+     */
 
     public void addUser(User user) {
       this.users.putIfAbsent(user, new ArrayList<>());
     }
+
+    /**
+     * Method finds the User by passport and adds the account to list of accounts.
+     * @param passport the passport by finds the user.
+     * @param account the account which add to the list
+     */
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -18,6 +41,12 @@ public class BankService {
             this.users.get(user).add(account);
         }
     }
+
+    /**
+     * finds the user by his passport.
+     * @param passport the passport by
+     * @return returns the user that was found.
+     */
 
     public User findByPassport(String passport) {
         User result = null;
@@ -29,6 +58,13 @@ public class BankService {
         }
         return result;
     }
+
+    /**
+     * Finds the user by passport and finds the account in the list
+     * @param passport the passport by finds the user.
+     * @param requisite the requisite by finds the account
+     * @return returns the account that was found.
+     */
 
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
@@ -43,6 +79,18 @@ public class BankService {
         }
         return rsl;
     }
+
+    /**
+     * Method finds source account and destination account.
+     * if sours account and destination account dont equals null
+     * method transfer money from sours account to destination account.
+     * @param srcPassport the passport by finds the srcUser.
+     * @param srcRequisite the requisite by finds the srcAccount.
+     * @param destPassport the passport by finds the destUser.
+     * @param destRequisite the requisite by finds the destAccount.
+     * @param amount the amount which transfer from srcAccount to destAccount.
+     * @return true if the operation was successful.
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
