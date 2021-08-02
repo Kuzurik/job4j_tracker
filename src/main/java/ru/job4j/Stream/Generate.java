49113b8743ba0f96873eefca98dcaa92396df7f1
kuzurik <vorota-24@bk.ru>
2021-08-02 09:33:30 +0300

    8. Перенос методов в Stream API [#279131]

--- src/main/java/ru/job4j/Stream/Generate.java
+++ src/main/java/ru/job4j/Stream/Generate.java
@@ -1,18 +1,12 @@
 package ru.job4j.Stream;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Generate {
 
     public static void main(String[] args) {
-        List<Card> cardDeck = new ArrayList<>();
         Stream.of(Suit.values()).flatMap(suit -> Stream.of(Value.values())
                 .map(value -> new Card(suit, value)))
-                .forEach(cardDeck::add);
-        cardDeck.forEach(System.out::println);
-
+                .forEach(System.out::println);
     }
 }
